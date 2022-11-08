23d7b58344a9615014c19d832149ca37d779b18a
AlessandroCristiano <alessandrocristiano66@gmail.com>
2022-11-08 15:33:13 +0100

    implementazione findbyexample e visualizza regista

---
+++ src/main/java/it/prova/raccoltafilm/web/servlet/regista/PrepareUpdateRegistaServlet.java
@@ -0,0 +1,24 @@
+package it.prova.raccoltafilm.web.servlet.regista;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrepareUpdateRegistaServlet
+ */
+@WebServlet("/PrepareUpdateRegistaServlet")
+public class PrepareUpdateRegistaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String parametroRegistaDaModificare=request.getParameter("idRegista");
+		
+		
+	}
+
+}
